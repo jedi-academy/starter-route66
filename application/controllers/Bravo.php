f531f2be0d7ceae6f61feb8c20a44564840432ca
Castiel Li <castiel.andrew@gmail.com>
2018-01-31 11:37:20 -0800

    made Bravo link work, added Bravo Controller closes #2

---
+++ application/controllers/Bravo.php
@@ -0,0 +1,17 @@
+<?php
+class Bravo extends Application
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Homepage for our app
+     */
+    public function index() {
+        $this->show(1);
+    }
+
+}
