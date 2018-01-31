843ee4ab1501db77a35f02e082610c30c0983d40
Hamish Brindle <hamishbrindle@gmail.com>
2018-01-31 11:54:02 -0800

    Fixing Delta controller mix-up, closes #20
    Signed-off-by: Hamish Brindle <hamishbrindle@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -5,9 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['menu_choices'] = array(
     'menudata' => array(
         array('name' => 'Alpha', 'link' => '/able'),
-        array('name' => 'Bravo', 'link' => '#'),
+        array('name' => 'Bravo', 'link' => '/bravo'),
         array('name' => 'Charlie', 'link' => '/charlie/brown'),
-        array('name' => 'Delta', 'link' => '/delta/force'),
+        array('name' => 'Force', 'link' => '/delta/force'),
         array('name' => 'Echo', 'link' => '#'),
         array('name' => 'Foxtrot', 'link' => '#'),
         array('name' => 'Golf', 'link' => '#'),
