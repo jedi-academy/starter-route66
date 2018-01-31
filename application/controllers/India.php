eeb891509d8c10a5b44f25c39b4a6e8da201fec8
Castiel Li <castiel.andrew@gmail.com>
2018-01-31 12:44:37 -0800

    make link india, created controller india, made data folder, added img into data folder closes #9

---
+++ application/controllers/India.php
@@ -0,0 +1,25 @@
+<?php
+
+class India extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Homepage for our app
+     */
+    public function index() {
+      // The following should be a relative link to your image file above
+$source = '../data/27-512.png';
+// note that we could have referenced an image anywhere on our system
+
+// set the mime type for that image (jpeg, png, etc)
+header("Content-type: image/png");
+header('Content-Disposition: inline');
+readfile($source); // dish it
+    }
+
+}
