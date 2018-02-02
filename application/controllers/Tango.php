e479910e3add695594ea63bd7a7d668160ec2b60
Sam Collins <samcollins427@gmail.com>
2018-02-01 16:46:15 -0800

    Foxtrot controller done
    Signed-off-by: Sam Collins <samcollins427@gmail.com>

---
+++ application/controllers/Tango.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Must
+ *
+ * @author Sam Collins
+ */
+class Tango extends Application {
+    //put your code here
+    function __construct()
+	{
+		parent::__construct();
+	}
+        
+    public function index() {
+        $this->show(5);
+    }
+}
