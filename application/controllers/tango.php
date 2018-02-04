59d09e69d7be55d94dfbe2938b8dba99e91582d6
wozenwho <wilson.huu@gmail.com>
2018-02-04 12:12:52 -0800

    Implemented india utility controller

--- application/controllers/tango.php
+++ application/controllers/tango.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+A remapped controller class. Redirects from foxtrot in config/config.php
+to controllers/tango.php
+*/
 class tango extends Application {
 
     public function index() {
